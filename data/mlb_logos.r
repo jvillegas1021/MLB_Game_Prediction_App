58b9bf04e9e73aa5e1ad237934aa9692303b7f8d
jvillegas1021 <jvillegas1021@yahoo.com>
2026-03-31 14:26:30 -0400

    Add files via upload

---
+++ data/mlb_logos.r
@@ -0,0 +1,191 @@
+
+
+team_logos <- list(
+  "Arizona Diamondbacks" = "mlb_team_logos/Arizona_Diamondbacks.png",
+  "Atlanta Braves" = "mlb_team_logos/Atlanta_Braves.png",
+  "Baltimore Orioles" = "mlb_team_logos/Baltimore_Orioles.png",
+  "Boston Red Sox" = "mlb_team_logos/Boston_Red_Sox.png",
+  "Chicago White Sox" = "mlb_team_logos/Chicago_White_Sox.png",
+  "Chicago Cubs" = "mlb_team_logos/Chicago_Cubs.png",
+  "Cincinnati Reds" = "mlb_team_logos/Cincinnati_Reds.png",
+  "Cleveland Guardians" = "mlb_team_logos/Cleveland_Guardians.png",
+  "Colorado Rockies" = "mlb_team_logos/Colorado_Rockies.png",
+  "Detroit Tigers" = "mlb_team_logos/Detroit_Tigers.png",
+  "Houston Astros" = "mlb_team_logos/Houston_Astros.png",
+  "Kansas City Royals" = "mlb_team_logos/Kansas_City_Royals.png",
+  "Los Angeles Angels" = "mlb_team_logos/Los_Angeles_Angels.png",
+  "Los Angeles Dodgers" = "mlb_team_logos/Los_Angeles_Dodgers.png",
+  "Miami Marlins" = "mlb_team_logos/Miami_Marlins.png",
+  "Milwaukee Brewers" = "mlb_team_logos/Milwaukee_Brewers.png",
+  "Minnesota Twins" = "mlb_team_logos/Minnesota_Twins.png",
+  "New York Yankees" = "mlb_team_logos/New_York_Yankees.png",
+  "New York Mets" = "mlb_team_logos/New_York_Mets.png",
+  "Athletics" = "mlb_team_logos/Athletics.png",
+  "Philadelphia Phillies" = "mlb_team_logos/Philadelphia_Phillies.png",
+  "Pittsburgh Pirates" = "mlb_team_logos/Pittsburgh_Pirates.png",
+  "San Diego Padres" = "mlb_team_logos/San_Diego_Padres.png",
+  "San Francisco Giants" = "mlb_team_logos/San_Francisco_Giants.png",
+  "Seattle Mariners" = "mlb_team_logos/Seattle_Mariners.png",
+  "St. Louis Cardinals" = "mlb_team_logos/St_Louis_Cardinals.png",
+  "Tampa Bay Rays" = "mlb_team_logos/Tampa_Bay_Rays.png",
+  "Texas Rangers" = "mlb_team_logos/Texas_Rangers.png",
+  "Toronto Blue Jays" = "mlb_team_logos/Toronto_Blue_Jays.png",
+  "Washington Nationals" = "mlb_team_logos/Washington_Nationals.png"
+)
+
+
+team_colors <- list(
+  "Arizona Diamondbacks" = "#A71930",
+  "Atlanta Braves" = "#CE1141",
+  "Baltimore Orioles" = "#DF4601",
+  "Boston Red Sox" = "#BD3039",
+  "Chicago Cubs" = "#0E3386",
+  "Chicago White Sox" = "#27251F",
+  "Cincinnati Reds" = "#C6011F",
+  "Cleveland Guardians" = "#00385D",
+  "Colorado Rockies" = "#33006F",
+  "Detroit Tigers" = "#0C2340",
+  "Houston Astros" = "#002D62",
+  "Kansas City Royals" = "#004687",
+  "Los Angeles Angels" = "#BA0021",
+  "Los Angeles Dodgers" = "#005A9C",
+  "Miami Marlins" = "#00A3E0",
+  "Milwaukee Brewers" = "#12284B",
+  "Minnesota Twins" = "#002B5C",
+  "New York Mets" = "#002D72",
+  "New York Yankees" = "#132448",
+  "Athletics" = "#003831",
+  "Philadelphia Phillies" = "#E81828",
+  "Pittsburgh Pirates" = "#FDB827",
+  "San Diego Padres" = "#2F241D",
+  "San Francisco Giants" = "#FD5A1E",
+  "Seattle Mariners" = "#0C2C56",
+  "St. Louis Cardinals" = "#C41E3A",
+  "Tampa Bay Rays" = "#092C5C",
+  "Texas Rangers" = "#003278",
+  "Toronto Blue Jays" = "#134A8E",
+  "Washington Nationals" = "#AB0003"
+)
+
+team_stadiums <- list(
+    "Arizona Diamondbacks" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/chase11965.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/chase11964.jpg"
+    ),
+    "Atlanta Braves" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/04/truist21main2-scaled.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/04/truist21960-scaled.jpg"
+    ),
+    "Baltimore Orioles" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/02/camden09972.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/02/camden09951.jpg"
+    ),
+    "Boston Red Sox" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/fenway15955.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/fenway13955.jpg"
+    ),
+    "Chicago White Sox" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/uscell1867.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/uscell1861.jpg"
+    ),
+    "Chicago Cubs" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/wrig23main.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/wrig10968.jpg"
+    ),
+    "Cincinnati Reds" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/gab23main.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/gab11961.jpg"
+    ),
+    "Cleveland Guardians" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/prog16951.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/prog10978.jpg"
+    ),
+    "Colorado Rockies" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/coors23main.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/coors23main.jpg"
+    ),
+    "Detroit Tigers" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/com11950.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/com119501.jpg"
+    ),
+    "Houston Astros" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/minute-top.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/minute13970.jpg"
+    ),
+    "Kansas City Royals" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/kauff15952.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/kauff906.jpg"
+    ),
+    "Los Angeles Angels" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/02/angel15955.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/02/angel13957.jpg"
+    ),
+    "Los Angeles Dodgers" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/dodger16_top3.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/dodger15952.jpg"
+    ),
+    "Miami Marlins" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/marlins_topv2.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/marlins2012971.jpg"
+    ),
+    "Milwaukee Brewers" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/aff21953.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/miller1770.jpg"
+    ),
+    "Minnesota Twins" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/target23main.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/target1616.jpg"
+    ),
+    "New York Yankees" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/yank10950.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/yank10970.jpg"
+    ),
+    "New York Mets" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/citi17951.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/citi10955.jpg"
+    ),
+    "Athletics" = list(
+        day  = "https://img.mlbstatic.com/mlb-images/image/upload/t_16x9/t_w1536/mlb/dhhjx4ihxu1qblvfilfx.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2024/12/sutter25951.jpg"
+    ),
+    "Philadelphia Phillies" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/citbank958.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/citbank_topv2.jpg"
+    ),
+    "Pittsburgh Pirates" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/pnc10855.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/pnc_topv2.jpg"
+    ),
+    "San Diego Padres" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/petco17markwhitt.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/petco2007901.jpg"
+    ),
+    "San Francisco Giants" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/att17952.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/att2012998.jpg"
+    ),
+    "Seattle Mariners" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/tmobile23main.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/safeco11951.jpg"
+    ),
+    "St. Louis Cardinals" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/busch913.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/busch17951.jpg"
+    ),
+    "Tampa Bay Rays" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/trop2012962.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/trop2012963.jpg"
+    ),
+    "Texas Rangers" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/06/globe21950-1.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/06/globe21953.jpg"
+    ),
+    "Toronto Blue Jays" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/rogers14954.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/davezolis3.jpg"
+    ),
+    "Washington Nationals" = list(
+        day  = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/nats23950.jpg",
+        night = "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/nats15959.jpg"
+    )
+)
